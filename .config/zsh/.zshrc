2990e9bf491f70e2ef18c627b2957dff2d89c386
yurikrupnik <krupnik.yuri@gmail.com>
2025-01-05 08:47:26 +0200

    init commit

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -43,23 +43,5 @@ setopt hist_ignore_all_dups
 setopt hist_save_no_dups
 setopt hist_find_no_dups
 
-# alias code=zed
-# alias k=kubectl
-# alias kx=kubectx
-# alias lsl='eza --no-permissions --no-user --no-time --long'
-# alias ls='eza --no-permissions --no-user --no-time'
-# alias cat='bat --paging never --style plain'
-# alias fzfp='fzf --preview="bat --style numbers --color=always {}"'
-# alias d=docker
-# alias drmi='docker rmi $(docker images -aq) -f'
-# alias dclean='docker system prune -f && docker volume prune -f'
-# alias g=gcloud
-# alias pu=pulumi
-# alias p=pnpm
-# alias c=cargo
-# alias cdoc=cargo doc
-# alias cdoco=cargo doc --open
-
-
 autoload -Uz compinit && compinit
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
--- init.sh
+++ init.sh
@@ -1,11 +1,33 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Check if curl is installed
+if ! command -v curl &> /dev/null; then
+    echo "Error: curl is not installed. Please install it and try again."
+    exit 1
+fi
+
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # install homebrew
 #sh <(curl -L https://nixos.org/nix/install)
 
-brew bundle
+if command -v brew >/dev/null 2>&1; then
+  brew bundle --file brew/Brewfile
+else
+  echo "Error: Homebrew is not installed or not in PATH"
+  exit 1
+fi
 
-kubectl krew install kuttl
+#kubectl krew install kuttl
+if command -v rustup >/dev/null 2>&1; then
+  echo "Rustup is already installed"
+else
+  echo "Error: rustup is not installed or not in PATH"
+  exit 1
+fi
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
@@ -20,7 +42,7 @@ go install github.com/mitranim/gow@latest
 brew tap fermyon/tap
 brew install fermyon/tap/spin
 # rust
-cargo install nu --locked
+#cargo install nu --locked
 #cargo install cargo-binstall
 #cargo binstall cargo-generate
 #cargo binstall wasm-pack
